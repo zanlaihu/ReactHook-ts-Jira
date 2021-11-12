6b2824e6652a3aae01479c5bbdeeaf052e3eab57
赞同 <huzanlai.hzl@alibaba-inc.com>
2021-11-12 15:57:30 +0800

    login页面

--- jira/src/App.tsx
+++ jira/src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { ProjectListScreen } from 'pages/project-list';
+import { LoginScreen } from 'pages/login';
 
 function App() {
   return (
     <div className="App">
-      <ProjectListScreen/>
+      {/* <ProjectListScreen/> */}
+      <LoginScreen/>
     </div>
   );
 }

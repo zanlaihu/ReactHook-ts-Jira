d51801a87ab8d6dcde1f274eb4ffa6a400609317
赞同 <huzanlai.hzl@alibaba-inc.com>
2021-11-26 17:43:52 +0800

    5-7

--- jira/__json_server_mock__/middleware.js
+++ jira/__json_server_mock__/middleware.js
@@ -1,3 +1,4 @@
+// npm run json-server
 module.exports = (req, res, next) => {
   if (req.method === "POST" && req.path === "/login") {
     if (req.body.username === "jack" && req.body.password === "123456") {
